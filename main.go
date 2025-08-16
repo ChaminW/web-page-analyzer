6a2d064dcd8b9a276707df5179f8074a488d00ba
Chamin Wickramarathna <chaminwickramarathna@Chamins-Macbook.local>
2025-08-16 09:39:00 +0530

    -refactor main file

--- main.go
+++ main.go
@@ -1,20 +1,48 @@
 package main
 
 import (
-	"fmt"
+	"context"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
+
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
+	logger := logrus.New()
+	logger.SetFormatter(&logrus.JSONFormatter{})
+	logger.SetLevel(logrus.InfoLevel)
+
 	httpServer := &http.Server{
-		Addr:         ":8081",
+		Addr:         ":8089",
+		Handler:      nil,
 		ReadTimeout:  15 * time.Second,
 		WriteTimeout: 15 * time.Second,
 		IdleTimeout:  60 * time.Second,
 	}
 
-	if err := httpServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-		fmt.Println("Server error", err)
+	go func() {
+		logger.Info("Starting server on :8080")
+		if err := httpServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			logger.Fatalf("Server error: %v", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	logger.Info("Shutting down server.")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	if err := httpServer.Shutdown(ctx); err != nil {
+		logger.Errorf("Server forced to shutdown: %v", err)
 	}
+
+	logger.Info("Server exited")
 }
